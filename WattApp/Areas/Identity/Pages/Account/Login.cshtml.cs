6441d9915ea94ff87aadf9a913fe2fab16d8a8b4
hboch <hboch1@gmx.net>
2018-12-25 12:18:10 +0100

    Change page to German display

--- WattApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WattApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -44,7 +44,7 @@ namespace WattApp.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Angemeldet bleiben")]
             public bool RememberMe { get; set; }
         }
 
